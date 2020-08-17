b8b3894e34517c09a3ef88ab0c27bfd31b20c09d
Aasmin <aasmin.kainth@gmail.com>
2020-08-17 09:56:37 -0700

    removed compiled code

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,14 @@
 import oui.*;
 /**
- * fires the app 
+ * Organization: Panjab University . Western University	
+ * Email: akainth4@uwo.ca 
+ * Final Year University Research Project.
  */
+
 public class Client {
 
 	public static void main(String[] args) throws Exception {
 		DataManager.basedir = args[0];
-		App app = new App();	
+		App app = new App();	//fires the app 
 	}
 }
